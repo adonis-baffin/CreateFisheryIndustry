a0b8fdcca59fd0c7817aec0a62ebaceeedb89920
adonis-baffin <3329586162@qq.com>
2025-04-22 23:46:54 +0800

    渔业2.0base

--- src/main/java/plus/dragons/createintegratedfarming/common/logistics/basket/package-info.java
+++
@@ -1,8 +0,0 @@
-@FieldsAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package plus.dragons.createintegratedfarming.common.logistics.basket;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
--- src/main/java/plus/dragons/createintegratedfarming/common/ranching/coop/package-info.java
+++
@@ -1,8 +0,0 @@
-@FieldsAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package plus.dragons.createintegratedfarming.common.ranching.coop;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
