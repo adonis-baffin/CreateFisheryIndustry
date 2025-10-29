0b8067aeea1474a4a132a81bc59db72bc6ef9898
adonis-baffin <3329586162@qq.com>
2025-10-29 13:50:00 +0800

    准备捕鱼轴承

--- src/main/java/com/adonis/createfisheryindustry/client/CreateFisheryPartialModels.java
+++ src/main/java/com/adonis/createfisheryindustry/client/CreateFisheryPartialModels.java
@@ -6,6 +6,7 @@ import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 public class CreateFisheryPartialModels {
 
     public static final PartialModel THRESHER_BLADE = block("mechanical_peeler/blade");
+    public static final PartialModel TRAP_BEARING_TOP = block("trap_bearing_top");
 
     private static PartialModel block(String path) {
         return PartialModel.of(CreateFisheryMod.asResource("block/" + path));
